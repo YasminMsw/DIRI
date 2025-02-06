5679ed45498b77c9d432191251d18c12a41f445d
YasminMsw <ymoussaoui15@gmail.com>
2025-02-06 19:59:08 +0100

    Retroalimentación sin Redux

--- tienda-comida/src/components/FoodOrder/FoodOrder.tsx
+++ tienda-comida/src/components/FoodOrder/FoodOrder.tsx
@@ -18,8 +18,10 @@ interface FoodOrderProps {
 }
 function FoodOrder(props: FoodOrderProps) {
   const dispatch = useDispatch<AppDispatch>();
-  const { loading, error } = useSelector((state: RootState) => state.order);
-
+  // const { loading, error } = useSelector((state: RootState) => state.order);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+ 
   const [quantity, setQuantity] = useState(1); // Estado para la cantidad seleccionada
   const [showConfirmation, setShowConfirmation] = useState(false); // Estado para mostrar el mensaje de confirmación
   const [isOrdered, setIsOrdered] = useState(false); // si está pedido
@@ -35,8 +37,11 @@ function FoodOrder(props: FoodOrderProps) {
   };
   const handleOrderSubmit = async() => {
    // props.onQuantityUpdated(props.food.id, quantity);
-    handleClick();
-    console.log("hola");
+
+   setLoading(true); // Indicar que la operación ha comenzado
+   setError(null); // Resetear el error antes de intentar la operación
+  handleClick();
+   
 
     setShowConfirmation(true); // Muestra el mensaje de confirmación
     const orderData = {
@@ -45,10 +50,19 @@ function FoodOrder(props: FoodOrderProps) {
       price: totalPrice,
       date: new Date().toISOString(), // Guarda la fecha del pedido
     };
-    console.log("hola")
+    //dispatch(addOrderAsync(orderData));
     //guardar el pedido en firebase
-    dispatch(addOrderAsync(orderData));
+    
+    try {
+      await addOrder(orderData); // Guardar en Firebase
+      logger.info(`Pedido enviado a Firebase`);
+    } catch (err) {
+      logger.error("Error al guardar el pedido en Firebase");
 
+      setError("No se pudo guardar el pedido. Inténtalo de nuevo.");
+    } finally {
+      setLoading(false); // Terminar la operación
+    }
   
   };
   const menuItems: MenuItem[] = useContext(foodItemsContext);
@@ -82,7 +96,8 @@ function FoodOrder(props: FoodOrderProps) {
 
       {/* Botón para enviar el pedido */}
       <button onClick={handleOrderSubmit} className="submitOrderButton">
-        Enviar Pedido
+        {loading ? "Guardando..." : "Enviar Pedido"}
+
       </button>
 
       {/* Mensaje de confirmación */}
@@ -96,8 +111,8 @@ function FoodOrder(props: FoodOrderProps) {
       <button onClick={props.onReturnToMenu} className="returnButton">
         Volver al Menú
       </button>
-      {loading && <p>Guardando pedido...</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}  
+      {loading && <p style={{ color: "blue" }}>Guardando pedido...</p>}
+      {error && <p style={{ color: "red" }}>{error}</p>}
     </div>
   );
 }
